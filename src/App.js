534fea393add60b689d80116773fdb910caa747d
varunharikumar <varun.h@stackontech.com>
2023-12-10 17:56:57 +0530

    Notes creating project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/pages/home';
+import Search from './components/pages/search';
+
+import Sidebar from './components/sidebar/sidebar';
+import Task from './components/pages/task'
+import Notes from "./components/pages/notes"
+import Archive from "./components/pages/archive"
+import Bin from "./components/pages/bin"
+import { TaskProvider } from './Contexts/task.context';
+import { NoteProvider } from './Contexts/note.context';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <TaskProvider>
+      <NoteProvider>
+      <div className="App">
+        <Sidebar></Sidebar>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/search" element={<Search />}></Route>
+          <Route exact path="/notes" element={<Notes />}></Route>
+          <Route exact path="/task" element={<Task />}></Route>
+          <Route exact path="/archive" element={<Archive />}></Route>
+          <Route exact path="/bin" element={<Bin />}></Route>
+        </Routes>
+      </div>
+      </NoteProvider>
+      </TaskProvider>
+    </Router>
   );
 }
 
